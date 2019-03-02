8bdfa96c6493853954407855a21dea8bc9217c4d
Anton Danchanka <gudvenby584@gmail.com>
2019-03-02 22:48:22 +0300

    Добавление основных окон приложения

---
+++ Project/SemCycle/SemCycle/DisciplineNotes.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SemCycle
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DisciplineNotes : ContentPage
+    {
+        public DisciplineNotes()
+        {
+            NavigationPage.SetHasNavigationBar(this, true);
+            Title = "2 Семестр | ОАИП";
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project/SemCycle/SemCycle/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SemCycle
+{
+    public partial class MainPage : ContentPage
+    {
+
+        string[][] terms = new string[8][];
+
+        public MainPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+            terms[0] = new string[] { "ОАиП", "Математика", "Физика", "История" };
+            terms[1] = new string[] { "АиЛОВТ", "ILOVEiT", "ОАиП", "Математика", "Физика" };
+        }
+
+        private void FirstSem_Clicked(object sender, EventArgs e)
+        {
+            IList<View> list = Header.Children;
+            foreach (View view in list)
+            {
+
+                (view as Button).BackgroundColor = Color.FromHex("#5677FC");
+            }
+
+            ((Button)sender).BackgroundColor = Header.BackgroundColor;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if ((Button)list[i] == (Button)sender)
+                {
+                    Term.ItemsSource = terms[i];
+                    //Term.ItemTapped += SemTapped;
+
+                }
+            }
+            Term.ItemTapped += ToNext;
+        }
+        private void SemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ToNext(sender, e);
+        }
+        private async void ToNext(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new MenuPage());
+        }
+
+
+    }
+}
