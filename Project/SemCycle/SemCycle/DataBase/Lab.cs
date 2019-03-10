4b18aae6ec1236d2845ae761eafed00127cde99c
Anton Danchanka <gudvenby584@gmail.com>
2019-03-10 17:29:56 +0300

    Добавление скелета

---
+++ Project/SemCycle/SemCycle/DataBase/Lab.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemCycle.DataBase
+{
+    class Lab
+    {
+        protected DateTime deadline;
+        protected string name { get => name; set => name = value; }
+        protected string note { set => note = value; }
+        protected int importance { get => importance; set => importance = value; }
+
+        public void update()
+        {
+
+        }
+        public void setDeadLine(DateTime time)
+        {
+            this.deadline = time;
+        }
+    }
+}
